cd74bd536b4db370a8d51228ba8391e55a7b3872
Luigig8per <luisma@g8perhead.com>
2017-06-23 14:46:44 -0600

    Guardado en varias DB
    Antes de crear instalador

---
+++ CACtiveMQSubscribe/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CACtiveMQSubscribe
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public List<line> GetLines()
+        {
+            using (DonBestEntities db = new DonBestEntities())
+
+
+                return db.lines.ToList();
+
+
+
+        }
+
+        private void getDatos()
+        {
+            List<line> colec = GetLines();
+
+            foreach (var item in colec)
+                label1.Text += "= Linea: " + item.ToString(); 
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            getDatos();
+        }
+    }
+}
--- CACtiveMQSubscribe/Program.cs
+++ CACtiveMQSubscribe/Program.cs
@@ -20,7 +20,7 @@ namespace CACtiveMQSubscribe
             // Read all messages off the queue
             while (ReadNextMessage())
             {
-                Console.WriteLine("Successfully read message");
+                //Console.WriteLine("Successfully read message");
             }
 
             Console.WriteLine("Finished");
@@ -70,7 +70,7 @@ namespace CACtiveMQSubscribe
                         ITextMessage txtMsg = msg as ITextMessage;
                         string body = txtMsg.Text;
 
-                        Console.WriteLine($"Received message: {txtMsg.Text}");
+                        //Console.WriteLine($"Received message: {txtMsg.Text}");
 
                         var xdoc = XDocument.Parse(body);
 
@@ -78,7 +78,7 @@ namespace CACtiveMQSubscribe
 
                         {
                             cont += 1;
-                            Console.WriteLine("Nodo " +  el.Name + ":" + el.Value + " " + el.Name);
+                            //Console.WriteLine("Nodo " +  el.Name + ":" + el.Value + " " + el.Name);
 
                             if (cont==1)
                             { 
@@ -90,7 +90,7 @@ namespace CACtiveMQSubscribe
                             {
                                 query += attr.Name + ",";
 
-                                if (attr.Name=="timestamp")
+                                if (attr.Name=="timestamp" || attr.Name == "time" ||  attr.Name == "message_timestamp")
                                 {
                                     var now = DateTime.Now; // Current date/time
                                     var utcNow = now.ToUniversalTime(); //
@@ -120,7 +120,7 @@ namespace CACtiveMQSubscribe
 
                                 query2 += "'" + attrValue + "',";
                                 //var itemValue = new ListViewItem(new[] { el.TagName, attr.Name, attr.Value });
-                                Console.WriteLine(attr.Name + ":" + attrValue + " " + el.Name);
+                                //Console.WriteLine(attr.Name + ":" + attrValue + " " + el.Name);
                                
                             }
 
@@ -133,7 +133,7 @@ namespace CACtiveMQSubscribe
                         query2=query2.Replace(",)", ")");
 
 
-                        Console.WriteLine(query + query2);
+                       
 
 
                         string connetionString = null;
@@ -149,7 +149,7 @@ namespace CACtiveMQSubscribe
                         try
                         {
                             connection2.Open();
-                            Console.WriteLine(" Connection Opened ");
+                            //Console.WriteLine(" Connection Opened ");
                             command = new SqlCommand(sql, connection2);
                             SqlDataReader dr1 = command.ExecuteReader();
 
@@ -158,6 +158,7 @@ namespace CACtiveMQSubscribe
                         catch (Exception ex)
                         {
                             Console.WriteLine(ex.Message);
+                            Console.WriteLine(query + query2);
                         }
 
 
@@ -172,13 +173,13 @@ namespace CACtiveMQSubscribe
                             XNode node = (XNode)myvar;
                             if (node.NodeType == XmlNodeType.Text)
                             {
-                                Console.WriteLine("Type = [" + node.NodeType + "] Value = " + node.ToString());
+                                //Console.WriteLine("Type = [" + node.NodeType + "] Value = " + node.ToString());
 
                             }
                             else
                             {
                                 XElement xdoc2 = new XElement((node as XElement).Name, (node as XElement).Value);
-                                Console.WriteLine("Type = [" + xdoc2.NodeType + "] Name = " + xdoc2.Name);
+                                //Console.WriteLine("Type = [" + xdoc2.NodeType + "] Name = " + xdoc2.Name);
                             }
                         }
                         
