594512772565298f5e92e69783eb9d502f6a6c4d
Luigig8per <luisma@g8perhead.com>
2017-06-23 13:04:15 -0600

    VB Layers error
    2 Errors

---
+++ CACtiveMQSubscribe/LineDAC.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CACtiveMQSubscribe
+{
+    class LineDAC
+    {
+        public List<line> GetLines()
+        {
+            using (DonBestEntities db = new DonBestEntities())
+
+
+                return db.lines.ToList();
+
+        }
+    }
+}
---
+++ Desktop/LineDk.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace Desktop
+{
+    partial class LineDk
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // LineDk
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Name = "LineDk";
+            this.Text = "LineDk";
+            this.Load += new System.EventHandler(this.LineDk_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
